1796cd14ba20b05a8fb5f5f1053361f855946b81
VieuxLoupGris <provocemois@gmail.com>
2019-02-13 07:26:12 +0100

    Fonction cat cicler with url

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-
 // import some polyfill to ensure everything works OK
 import "babel-polyfill"
 
 // import bootstrap's javascript part
 import 'bootstrap';
 
+import axios from 'axios';
+
 /*
   Put the JavaScript code you want below.
 */
 
 console.log("Hey look in your browser console. It works!");
+
+let cute_Cat = document.querySelector("#cute_Cat");
+
+async function getCuteCat() {
+
+  let answer = await axios.get('https://aws.random.cat/meow');
+  cute_Cat.src = answer.data.file;
+}
+setInterval(() => {
+
+  getCuteCat()
+}, 1000);
